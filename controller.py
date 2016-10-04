688c83b85b98979b1253a216528b926f740bb89c
EvaSuarezGarcia <eva.suarez.garci@gmail.com>
2016-10-04 18:17:30 +0200

    Agrupado el código en clase App

--- controller.py
+++ controller.py
@@ -22,23 +22,23 @@ locale.bindtextdomain("p1", LOCALE_DIR)
 gettext.bindtextdomain("p1", LOCALE_DIR)
 gettext.textdomain("p1")
 
-class Handler():
+class App():
     '''
-    Clase con los manejadores de la vista
+    Manejadores de la vista
     '''
 
     # Boton añadir pelicula
     def on_add(self, w):
 
         # Obtener dialogo añadir y sus entradas de texto y spinbuttons
-        danadir = builder.get_object("dialog1")
+        danadir = self.builder.get_object("dialog1")
         titdialog = _("Añadir película")
         danadir.set_title(titdialog)
 
-        etitulo = builder.get_object("entry1")
-        eano = builder.get_object("spinbutton1")
-        eduracion = builder.get_object("spinbutton2")
-        edirector = builder.get_object("entry2")
+        etitulo = self.builder.get_object("entry1")
+        eano = self.builder.get_object("spinbutton1")
+        eduracion = self.builder.get_object("spinbutton2")
+        edirector = self.builder.get_object("entry2")
 
         # Resetear contenido de las entradas
         etitulo.set_text("")
@@ -62,17 +62,17 @@ class Handler():
                 # Comprobar que la pelicula tenga titulo
                 if titulo == "":
                     # Dialogo informativo
-                    builder.get_object("messagedialog2").run()
-                    builder.get_object("messagedialog2").hide()
+                    self.builder.get_object("messagedialog2").run()
+                    self.builder.get_object("messagedialog2").hide()
                     continue
 
                 # Habilitar botones Eliminar y Editar si es necesario
-                enable_delete_cond()
-                enable_edit_cond()
+                self.enable_delete_cond()
+                self.enable_edit_cond()
 
                 # Añadir pelicula al modelo
                 movie = Movie(titulo, ano, duracion, director)
-                model.add_movie(movie)
+                self.model.add_movie(movie)
                 break
             else:
                 break
@@ -83,25 +83,25 @@ class Handler():
     # Boton eliminar pelicula
     def on_remove(self, w):
 
-        movie = builder.get_object("treeview1").get_selection().get_selected()[1]
+        movie = self.builder.get_object("treeview1").get_selection().get_selected()[1]
 
         # No hacer nada si no hay una pelicula seleccionada
         if movie == None:
             return
 
-        deliminar = builder.get_object("messagedialog1")
+        deliminar = self.builder.get_object("messagedialog1")
 
         # Preparar la label secundaria para que muestre el titulo de la pelicula
-        deliminar.get_message_area().get_children()[1].set_label(model.get_title(movie))
+        deliminar.get_message_area().get_children()[1].set_label(self.model.get_title(movie))
 
         # Correr el dialogo
         r = deliminar.run()
 
         # Si se selecciona ok
         if r == 0:
-            model.delete_movie(movie)
-            disable_delete_cond()
-            disable_edit_cond()
+            self.model.delete_movie(movie)
+            self.disable_delete_cond()
+            self.disable_edit_cond()
 
         # Esconder dialogo
         deliminar.hide()
@@ -109,7 +109,7 @@ class Handler():
     # Boton añadir pelicula
     def on_edit(self, w):
 
-        movie_iter = builder.get_object("treeview1").get_selection().get_selected()[1]
+        movie_iter = self.builder.get_object("treeview1").get_selection().get_selected()[1]
 
         # No hacer nada si no hay una pelicula seleccionada
         if movie_iter == None:
@@ -117,19 +117,19 @@ class Handler():
 
         # Obtener dialogo editar y sus entradas de texto y spinbuttons
         # Se reutiliza el dialogo de añadir
-        deditar = builder.get_object("dialog1")
+        deditar = self.builder.get_object("dialog1")
         titdialog = _("Editar película")
         deditar.set_title(titdialog)
-        etitulo = builder.get_object("entry1")
-        eano = builder.get_object("spinbutton1")
-        eduracion = builder.get_object("spinbutton2")
-        edirector = builder.get_object("entry2")
+        etitulo = self.builder.get_object("entry1")
+        eano = self.builder.get_object("spinbutton1")
+        eduracion = self.builder.get_object("spinbutton2")
+        edirector = self.builder.get_object("entry2")
 
         # Se muestran las entradas de texto y spinbuttons con los datos originales de la pelicula
-        etitulo.set_text(model.get_title(movie_iter))
-        eano.set_value(int(model.get_year(movie_iter)))
-        eduracion.set_value(int(model.get_duration(movie_iter)))
-        edirector.set_text(model.get_director(movie_iter))
+        etitulo.set_text(self.model.get_title(movie_iter))
+        eano.set_value(int(self.model.get_year(movie_iter)))
+        eduracion.set_value(int(self.model.get_duration(movie_iter)))
+        edirector.set_text(self.model.get_director(movie_iter))
 
         # Esperar por una respuesta valida (aceptar con datos correctos o cancelar)
         while True:
@@ -147,13 +147,13 @@ class Handler():
                 # Comprobar que la pelicula tenga titulo
                 if titulo == "":
                     # Dialogo informativo
-                    builder.get_object("messagedialog2").run()
-                    builder.get_object("messagedialog2").hide()
+                    self.builder.get_object("messagedialog2").run()
+                    self.builder.get_object("messagedialog2").hide()
                     continue
 
                 # Añadir pelicula al modelo
                 movie = Movie(titulo, ano, duracion, director)
-                model.set_movie(movie_iter, movie)
+                self.model.set_movie(movie_iter, movie)
                 break
             else:
                 break
@@ -163,46 +163,54 @@ class Handler():
 
     # Cierre de aplicacion
     def on_close(self, w, e):
-        model.save()
+        self.model.save()
         Gtk.main_quit()
 
-# Deshabilitar boton de eliminar si no hay peliculas
-def disable_delete_cond():
-    if model.is_empty():
-        builder.get_object("button2").set_sensitive(False)
+    '''
+    Resto de elementos de la aplicacion
+    '''
 
-# Habilitar boton de eliminar si empieza a haber peliculas
-def enable_delete_cond():
-    if model.is_empty():
-        builder.get_object("button2").set_sensitive(True)
+    # Deshabilitar boton de eliminar si no hay peliculas
+    def disable_delete_cond(self):
+        if self.model.is_empty():
+            self.builder.get_object("button2").set_sensitive(False)
 
-# Deshabilitar boton de editar si no hay peliculas
-def disable_edit_cond():
-    if model.is_empty():
-        builder.get_object("button3").set_sensitive(False)
+    # Habilitar boton de eliminar si empieza a haber peliculas
+    def enable_delete_cond(self):
+        if self.model.is_empty():
+            self.builder.get_object("button2").set_sensitive(True)
 
-# Habilitar boton de editar si empieza a haber peliculas
-def enable_edit_cond():
-    if model.is_empty():
-        builder.get_object("button3").set_sensitive(True)
+    # Deshabilitar boton de editar si no hay peliculas
+    def disable_edit_cond(self):
+        if self.model.is_empty():
+            self.builder.get_object("button3").set_sensitive(False)
 
-# Se crea el modelo
-model = ListMovie()
+    # Habilitar boton de editar si empieza a haber peliculas
+    def enable_edit_cond(self):
+        if self.model.is_empty():
+            self.builder.get_object("button3").set_sensitive(True)
 
-# Se crean las vistas
-builder = Gtk.Builder()
-# Dominio de traduccion
-builder.set_translation_domain("p1")
-builder.add_from_file("view.glade")
-builder.connect_signals(Handler())
+    def __init__(self):
+        # Se crea el modelo
+        self.model = ListMovie()
 
-# Establecer modelo en la vista
-builder.get_object("treeview1").set_model(model.listmodel)
+        # Se crean las vistas
+        self.builder = Gtk.Builder()
+        # Dominio de traduccion
+        self.builder.set_translation_domain("p1")
+        self.builder.add_from_file("view.glade")
+        self.builder.connect_signals(self)
 
-# Desactivar Eliminar y Editar si es necesario
-disable_delete_cond()
-disable_edit_cond()
+        # Establecer modelo en la vista
+        self.builder.get_object("treeview1").set_model(self.model.listmodel)
 
-mainwin = builder.get_object("window1")
-mainwin.show_all()
+        # Desactivar Eliminar y Editar si es necesario
+        self.disable_delete_cond()
+        self.disable_edit_cond()
+
+        mainwin = self.builder.get_object("window1")
+        mainwin.show_all()
+
+# Ejecucion
+App()
 Gtk.main()
